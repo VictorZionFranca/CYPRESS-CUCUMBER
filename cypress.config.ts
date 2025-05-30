839ea5115c786ff86b3494d31e377ec8484d96d8
VictorZionFranca <vzion435@gmail.com>
2025-05-30 14:34:53 -0300

    Reports

---
+++ cypress.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'cypress';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
+import mochawesome from 'cypress-mochawesome-reporter/plugin';
+
+export default defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      mochawesome(on);
+
+      return config;
+    },
+    specPattern: 'cypress/e2e/**/*.feature',
+    supportFile: 'cypress/support/e2e.ts',
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+});
---
+++ global.d.ts
@@ -0,0 +1,4 @@
+declare module 'cypress-mochawesome-reporter/plugin' {
+  const plugin: (on: any, config?: any) => void;
+  export default plugin;
+}
